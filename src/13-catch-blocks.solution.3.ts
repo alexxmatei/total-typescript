5eb8bffed2bb85bd2b01b91f18f2787b34e8be9c
alexxmatei <alexxmatei@gmail.com>
2023-03-19 15:18:58 +0200

    Add alternate solutions for e-13

---
+++ src/13-catch-blocks.solution.3.ts
@@ -0,0 +1,18 @@
+import { expect, it } from "vitest";
+
+const tryCatchDemo = (state: "fail" | "succeed") => {
+  try {
+    if (state === "fail") {
+      throw new Error("Failure!");
+    }
+  } catch (e) {
+    /* Recommended solution out of the 3 */
+    if (e instanceof Error) {
+      return e.message;
+    }
+  }
+};
+
+it("Should return the message when it fails", () => {
+  expect(tryCatchDemo("fail")).toEqual("Failure!");
+});
