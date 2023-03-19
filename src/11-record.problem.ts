c5caf8a86e9e858d0bb4a43c6aa69342692a77df
alexxmatei <alexxmatei@gmail.com>
2023-03-19 14:21:40 +0200

    Remove duplicate reference

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -4,7 +4,6 @@ const createCache = () => {
   const cache: { [id: string]: string } = {};
 
   /* REF: https://stackoverflow.com/questions/56833469/typescript-error-ts7053-element-implicitly-has-an-any-type
-          https://stackoverflow.com/questions/56833469/typescript-error-ts7053-element-implicitly-has-an-any-type
           https://www.totaltypescript.com/tutorials/beginners-typescript/assigning-dynamic-keys-to-an-object/solution */
   const add = (id: string, value: string) => {
     cache[id] = value;
